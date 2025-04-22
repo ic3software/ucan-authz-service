fa79cd792079ccf71544f5c4c9f63f15d1a6bbf3
vthwang <vincent@fishboneapps.com>
2025-04-21 23:33:29 -0700

    feat: add delegation feature
    Signed-off-by: vthwang <vincent@fishboneapps.com>

---
+++ src/lib/keys.ts
@@ -0,0 +1,34 @@
+import type { DidableKey } from "@ucans/ucans"
+import { exportPublicKey } from "./crypto"
+import * as uint8arrays from "uint8arrays"
+
+export async function toDidableKey(keypair: CryptoKeyPair): Promise<DidableKey> {
+    const publicKey = keypair.publicKey
+    const privateKey = keypair.privateKey
+
+    const publicKeyBytes = await crypto.subtle.exportKey('raw', publicKey)
+    const did = publicKeyToDid(new Uint8Array(publicKeyBytes))
+
+    console.log('did', did);
+
+    return {
+        jwtAlg: 'EdDSA',
+        sign: async (data: Uint8Array) => {
+            const sig = await crypto.subtle.sign({ name: 'Ed25519' }, privateKey, data)
+            return new Uint8Array(sig)
+        },
+        did: () => did
+    }
+}
+
+export const EDWARDS_DID_PREFIX = new Uint8Array([0xed, 0x01])
+
+export const publicKeyToDid = (pubkey: Uint8Array): string => {
+    return didFromKeyBytes(pubkey, EDWARDS_DID_PREFIX)
+}
+
+export function didFromKeyBytes(publicKeyBytes: Uint8Array, prefix: Uint8Array): string {
+    const bytes = uint8arrays.concat([prefix, publicKeyBytes])
+    const base58Key = uint8arrays.toString(bytes, "base58btc")
+    return "did:key:z" + base58Key
+}
---
+++ src/routes/delegate/+page.server.ts
@@ -0,0 +1,13 @@
+import * as ucans from "@ucans/ucans"
+import { PRIVATE_SERVER_KEY } from '$env/static/private';
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async () => {
+    if (!PRIVATE_SERVER_KEY) {
+        throw new Error('Server DID key is not configured');
+    }
+
+    const keypair = ucans.EdKeypair.fromSecretKey(PRIVATE_SERVER_KEY);
+    const serverDidKey = keypair.did();
+    return { serverDidKey };
+};
